858b8ea16f126aa98341b17b3dc0b6d5e782e487
Stephanie Cruz <scruz.17@est.ucab.edu.ve>
2020-11-19 13:03:23 -0400

    Agregar funciones basicas para el funcionamiento del programa

--- constants.py
+++ constants.py
@@ -1,1 +1,16 @@
 PIZZA_GRANDE = 8
+
+import enum
+class Sizes(enum.Enum): # Tamaños de las pizzas
+   Grande = 'g'
+   Mediano = 'm'
+   Personal = 'p'
+
+class Ingredients(enum.Enum): # Tamaños de las pizzas
+   Jamon = 'ja'
+   Champiñones = 'ch'
+   Pimentón = 'pi'
+   DobleQueso = 'dq'
+   Aceitunas = 'ac'
+   Pepperoni = 'pp'
+   Salchichón = 'sa'
--- main.py
+++ main.py
@@ -6,9 +6,24 @@ print(' /      |_______________________________|     \ ')
 print('/__________)                        (__________\ ')
 
 import constants
+import utility_functions as fn
+
+
+  
+
 def main():
-    print('Realizar una nueva orden')
-    print(constants.PIZZA_GRANDE)
+    quantity = 0
+    orden = input('Presione "s" para realizar una nueva orden: ')
+    orden = fn.yes_or_not("Desea realizar una nueva orden? (s/n). Debe seleccionar una opcion correcta : ",orden)
+    print(orden)
+    if orden == 's':
+        quantity = 1
+        print('Pizza número: ',quantity)
+    print(fn.give_options('inline','Tamaño',constants.Sizes)) 
+    print(fn.give_options('not-inline','Ingredientes',constants.Ingredients)) 
+
+  # print(constants.PIZZA_GRANDE)
+
 
 if __name__ == "__main__":
     main()
