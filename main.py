d705862aa4fc51e77c8acc87c11a1cdbc906d398
victorvieirap <victormvieira99@hotmail.com>
2020-11-19 16:58:10 -0400

    Agregar precios totales y subtotales

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import constants
 import utility_functions as fn
- 
+total_price = 0
  
 def welcome_print():
    print('{:<32}         _______________________________'.format(''))
@@ -9,30 +9,39 @@ def welcome_print():
    print('{:<32} \      |  |   __|__   /__ /__  /   \   |     /'.format(''))
    print('{:<32} /      |_______________________________|     \ '.format(''))
    print('{:<32}/__________)                        (__________\ '.format(''))
+
+def get_pizza_price(size,ingredients):
+   global total_price
+   sub_total = constants.Sizes[size].value[1]
+   for i in (constants.Ingredients):
+      for j in ingredients:
+         if i.name == j:
+               sub_total = sub_total + i.value[1]
+   print('\nSubtotal a pagar por una pizza '+size+' : '+str(sub_total)+'$ \n')
+   total_price = total_price + sub_total
+
  
 def order_pizza(quantity):
    print('\n')
    print('------------------------- Pizza número: ',quantity,'-------------------------')
-   size = fn.give_options('inline','Tamaño',constants.Sizes).name
+   size = fn.give_options('inline','Tamaño',constants.Sizes) #.name
    ingredients = fn.give_options('not-inline','Ingredientes',constants.Ingredients)
    print('\nUsted seleccionó una pizza '+size, end =" con ")
    for i in ingredients:
-       print(i.name, end=", ")
-   print('\n')
- 
+         print(i, end=", ")
+   get_pizza_price(size,ingredients)
+
 def main():
    welcome_print()
    quantity = 1
    orden = input('\nPresione "s" para realizar una nueva orden: ')
-   orden = fn.yes_or_not("Desea realizar una nueva orden? (s/n). Debe seleccionar una opcion correcta : ",orden)   
-   while orden == 's':        
-       order_pizza(quantity)
-       orden = input('Desea continuar (s/n)? ')
-       orden = fn.yes_or_not("Debe seleccionar una opcion correcta (s/n) ",orden)   
-       quantity = quantity+1
- 
-  
- 
- 
-if __name__ == "__main__":
+   orden = fn.yes_or_not("Desea realizar una nueva orden? (s/n). Debe seleccionar una opcion correcta : ",orden)  
+   while orden == 's':       
+         order_pizza(quantity)
+         orden = input('Desea continuar (s/n)? ')
+         orden = fn.yes_or_not("Debe seleccionar una opcion correcta (s/n) ",orden)  
+         quantity = quantity+1
+   if orden == 'n':
+         print('El pedido tiene un total de ',(quantity-1), ' pizzas, por un monto de ', total_price,'$')
+   if __name__ == "__main__":
    main()
--- utility_functions.py
+++ utility_functions.py
@@ -1,49 +1,50 @@
 def yes_or_not(message, result): # Verifica si la opción seleccionada es 's' o 'n' , e itera hasta que lo sea
-   while result not in ('s', 'n'):
-           result = input(message)
-   if result == 'n' or result == 's' :
-       return result   
- 
+    while result not in ('s', 'n'):
+            result = input(message)
+    if result == 'n' or result == 's' :
+        return result  
+
 def insert_into_array(array, item): # inserta en un array sin duplicados
-   into = False
-   if len(array) > 0:
-       for i in array:
-           if i == item:
-               into = True
-   if into == False:
-       array.append(item)
-   return array
- 
-def get_enum_key(options,option): # retorna el nombre, dado el valor del enum.
-   return options(option)
- 
+    into = False
+    if len(array) > 0:
+        for i in array:
+            if i == item:
+                into = True
+    if into == False:
+        array.append(item)
+    return array
+
+def get_enum_key(options,option,val): # retorna el nombre, dado el valor del enum. (Puede ser dado el acronimo o el precio)
+    for opt in (options):
+        if opt.value[val] == option:
+        return opt.name
+
 def give_options(format,title,options): # Dato un enum, lista las opciones disponibles e itera hasta que la respuesta sea válida. Puede darse el caso de que retorne un valor numerico, o una lista con valores (no repetidos), dependiendo de los parámetros pasados
- print('\n****** '+title+' : ******\n')
- text = ''
- for option in (options):
-     if format == 'inline':
-       text = text + option.name + ' ('+option.value+') '
-     else:
-         print(text + option.name + ' ('+option.value+') ')
- if format != 'inline':
-     array = []
-     print('\n')
-     text =  'Indique la opción (enter para terminar)'     
- current_option = input(text+' : ')
- correct = False
- while True:
-   if current_option == "":
-       return array
-   for option in (options):
-           if option.value == current_option:
-               if format == 'inline':
-                   return get_enum_key(options,current_option)
-               else:
-                   array = insert_into_array(array, get_enum_key(options,current_option))
-               correct = True
-   if correct == False:
-       current_option = input(text + '. Por favor, indique una respuesta válida : ')
-   else:
-       current_option = input(text+ ' : ')
-   correct = False
- 
+    print('\n****** '+title+' : ******\n')
+    text = ''
+    for option in (options):
+        if format == 'inline':
+        text = text + option.name + ' ('+option.value[0]+') '
+        else:
+            print(text + option.name + ' ('+option.value[0]+') ')
+    if format != 'inline':
+        array = []
+        print('\n')
+        text =  'Indique la opción (enter para terminar)'    
+    current_option = input(text+' : ')
+    correct = False
+    while True:
+    if current_option == "":
+        return array
+    for option in (options):
+            if option.value[0] == current_option:
+                if format == 'inline':
+                    return  get_enum_key(options,current_option,0)
+                else:
+                    array = insert_into_array(array, get_enum_key(options,current_option,0))
+                correct = True
+    if correct == False:
+        current_option = input(text + '. Por favor, indique una respuesta válida : ')
+    else:
+        current_option = input(text+ ' : ')
+    correct = False
